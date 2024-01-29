eb0643c2d026062a206b7106f1e41b03acbe8646
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2024-01-29 12:27:17 +0900

    feat: publishing Recommndation section

--- src/app/screens/HomePage/bestBook.tsx
+++ src/app/screens/HomePage/bestBook.tsx
@@ -8,25 +8,66 @@ export function BestBook() {
       <Container>
         <Stack flexDirection={"column"} alignItems={"center"}>
           <Box className={"top_book_title"}>Top Rated Books</Box>
-          
+          <Stack sx={{ flexDirection: "row", marginTop: '30px', justifyContent: "space-between" }}>
+            
+              <Box className={"book_box"} sx={{marginRight: "50px"}}>
+                <span className={"book_image"}>
+                  <img src={"/book/book.jpeg"} />
+                </span>
+                <span className="book_icons">
+                  <img src={"/icons/yulduzcha.svg"} />
+                  <p>Harry Potter</p>
+                  <p>John Deo</p>
+                </span>
+                <Box className="book_price_box">
+                  <span className="book_price">$19,23</span>
+                </Box>
+              </Box>
 
-        <Stack flexDirection={"row"} sx={{mt: "10px"}}>
-          <Box className={"book_box"}>
-          <span className={"book_image"}>
-            <img src={"/book/book.jpeg"} />
-            </span>
-            <span className="book_icons">
-              <img src={"/icons/yulduzcha.svg"} />
-              <p>Harry Potter</p>
-              <p>John Deo</p>
-            </span>
-            <Box className="book_price_box">
-              <span className="book_price">$19,23</span>
-            </Box>
-          </Box>
-        </Stack>
 
-        
+              <Box className={"book_box"} sx={{marginRight: "50px"}}>
+                <span className={"book_image"}>
+                  <img src={"/book/book.jpeg"} />
+                </span>
+                <span className="book_icons">
+                  <img src={"/icons/yulduzcha.svg"} />
+                  <p>Harry Potter</p>
+                  <p>John Deo</p>
+                </span>
+                <Box className="book_price_box">
+                  <span className="book_price">$19,23</span>
+                </Box>
+              </Box>
+
+              <Box className={"book_box"} sx={{marginRight: "50px"}}>
+                <span className={"book_image"}>
+                  <img src={"/book/book.jpeg"} />
+                </span>
+                <span className="book_icons">
+                  <img src={"/icons/yulduzcha.svg"} />
+                  <p>Harry Potter</p>
+                  <p>John Deo</p>
+                </span>
+                <Box className="book_price_box">
+                  <span className="book_price">$19,23</span>
+                </Box>
+              </Box>
+
+              <Box className={"book_box"} sx={{marginRight: "50px"}}>
+                <span className={"book_image"}>
+                  <img src={"/book/book.jpeg"} />
+                </span>
+                <span className="book_icons">
+                  <img src={"/icons/yulduzcha.svg"} />
+                  <p>Harry Potter</p>
+                  <p>John Deo</p>
+                </span>
+                <Box className="book_price_box">
+                  <span className="book_price">$19,23</span>
+                </Box>
+              </Box>
+           
+          </Stack>
         </Stack>
       </Container>
     </div>
--- src/app/screens/HomePage/recommendation.tsx
+++ src/app/screens/HomePage/recommendation.tsx
@@ -1,10 +1,81 @@
 import React from "react";
-import {Container} from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
 
 export function Recommendation() {
-    return (
-        <div className="recomend_frame"> 
-        <Container>Recommendation</Container> 
-        </div>
-    );
-} 
+  return (
+    <div className="recomend_frame">
+      <Container>
+        <Stack className="recomendation_box" sx={{ flexDirection: "column" }}>
+          <Box className="recomendation_top">
+            <h1>Recommendation</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </p>
+          </Box>
+
+          <Stack className="recom_middle_box" sx={{ flexDirection: "row"}}>
+            <Stack className="recom_main_box" sx={{ flexDirection: "column", marginRight: "40px" }}>
+              <Box className="img_box">
+                <img src="/book/poem.jpeg"></img>
+              </Box>
+              <Box className="book_def">
+                <h4>Why reading is important for our children?</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do
+                  eiusmod tempor incididunt ut labore et dolore...
+                </p>
+              </Box>
+              <Box className="author_book">
+                <Box>
+                  <img src="/icons/odamcha.png"></img>
+                  <p>James Wong</p>
+                </Box>
+              </Box>
+            </Stack>
+
+            <Stack className="recom_main_box" sx={{ flexDirection: "column", marginRight: "40px"}}>
+              <Box className="img_box">
+                <img src="/book/poem.jpeg"></img>
+              </Box>
+              <Box className="book_def">
+                <h4>Why reading is important for our children?</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do
+                  eiusmod tempor incididunt ut labore et dolore...
+                </p>
+              </Box>
+              <Box className="author_book">
+                <Box>
+                  <img src="/icons/odamcha.png"></img>
+                  <p>James Wong</p>
+                </Box>
+              </Box>
+            </Stack>
+
+            <Stack className="recom_main_box" sx={{ flexDirection: "column",marginRight: "40px",}}>
+              <Box className="img_box">
+                <img src="/book/poem.jpeg"></img>
+              </Box>
+              <Box className="book_def">
+                <h4>Why reading is important for our children?</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do
+                  eiusmod tempor incididunt ut labore et dolore...
+                </p>
+              </Box>
+              <Box className="author_book">
+                <Box>
+                  <img src="/icons/odamcha.png"></img>
+                  <p>James Wong</p>
+                </Box>
+              </Box>
+            </Stack>
+
+          </Stack>
+          
+        </Stack>
+      </Container>
+    </div>
+  );
+}
